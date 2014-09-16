b85a361ecd7cdac025e0811f4f79f243fc84e520
James Reeves <jreeves@weavejester.com>
2014-09-16 11:14:20 -0400

    Release 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-jetty-component "0.2.0"
+(defproject ring-jetty-component "0.2.1"
   :description "A component for the standard Ring Jetty adapter"
   :url "https://github.com/weavejester/ring-jetty-component"
   :license {:name "The MIT License"
