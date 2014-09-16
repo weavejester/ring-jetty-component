3d29c36763222261be8c56257363e3869aa7912b
James Reeves <jreeves@weavejester.com>
2014-09-16 11:11:09 -0400

    Update Component dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.stuartsierra/component "0.2.1"]
+                 [com.stuartsierra/component "0.2.2"]
                  [ring/ring-jetty-adapter "1.3.1"]]
   :aliases {"test-all" ["do" ["test"] ["with-profile" "+1.6" "test"]]}
   :profiles
