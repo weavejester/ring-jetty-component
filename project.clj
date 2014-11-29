ddd360db9575134977f109db497127a910aefd0a
James Reeves <jreeves@weavejester.com>
2014-11-29 20:47:06 +0000

    Update ring-jetty-adapter to 1.3.2

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.stuartsierra/component "0.2.2"]
-                 [ring/ring-jetty-adapter "1.3.1"]]
+                 [ring/ring-jetty-adapter "1.3.2"]]
   :aliases {"test-all" ["do" ["test"] ["with-profile" "+1.6" "test"]]}
   :profiles
   {:dev {:dependencies [[clj-http "1.0.0"]]}
